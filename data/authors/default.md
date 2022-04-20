bfe71b762592a4a44ebfe55d8fb1541bfd87a254
Todd Christensen <toddchristensen@protonmail.com>
2022-04-20 14:11:43 +0800

    first commit

---
+++ data/authors/default.md
@@ -0,0 +1,18 @@
+---
+name: Todd Christensen
+avatar: /static/images/avatar.webp
+occupation: Web Developer
+company: Open to job offers
+email: hello@toddchristensen.net
+twitter: https://twitter.com/christensen_tr
+linkedin: https://www.linkedin.com
+github: https://github.com/trchristensen
+excerpt: Realizing my ideas through code. California native, currently residing in the Philippines 🇵🇭
+
+---
+
+Todd Christensen is talking about himself in the third person right now. He is a web developer with plenty of experience in front end applications and websites. His most used frontend framework is React, with Next.js. Obviously he can build a full stack application. 
+
+Todd is currently working on implementing blockchain technologies into his projects. In his expert opinion, decentralization is critical to the future of the web.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
---
+++ layouts/AuthorLayout.Home.tsx
@@ -0,0 +1,55 @@
+import SocialIcon from '@/components/social-icons'
+import Image from '@/components/Image'
+import { PageSEO } from '@/components/SEO'
+import { ReactNode } from 'react'
+import { AuthorFrontMatter } from 'types/AuthorFrontMatter'
+
+interface Props {
+  children: ReactNode
+  frontMatter: AuthorFrontMatter
+}
+
+export default function AuthorLayout({ children, frontMatter }: Props) {
+  const { name, avatar, occupation, company, email, twitter, linkedin, github, excerpt } =
+    frontMatter
+
+  return (
+    <>
+      <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
+      <div className="">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5"></div>
+        <div className="flex flex-col-reverse items-start sm:flex-row">
+          <div className="flex flex-col items-start space-x-2 pt-8">
+            <h1 className="text-primary mb-1 text-3xl font-bold tracking-tight md:text-5xl">
+              {name}
+            </h1>
+            {/* <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3> */}
+            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+            <div className="text-gray-500 dark:text-gray-400">
+              {company}
+              <div className="mx-2 inline-block h-3 w-3 rounded-full bg-green-500"></div>
+            </div>
+            <div className="flex space-x-3 pt-6">
+              <SocialIcon kind="mail" href={`mailto:${email}`} />
+              <SocialIcon kind="github" href={github} />
+              <SocialIcon kind="linkedin" href={linkedin} />
+              <SocialIcon kind="twitter" href={twitter} />
+            </div>
+            <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
+              {excerpt}
+            </div>
+          </div>
+          <div>
+            <Image
+              src={avatar}
+              alt="avatar"
+              width="192px"
+              height="192px"
+              className="h-48 w-48 rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
